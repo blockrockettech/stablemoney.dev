878e83f31c5b706eded63b05f4676128d0a6486f
James Morgan <james.morgan.e@googlemail.com>
2026-03-29 21:29:07 +0100

    bug: switch all sitemap frequency updates to weekly

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -24,13 +24,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: url("/standards", root),
       lastModified,
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 0.85,
     },
     {
       url: url("/compare", root),
       lastModified,
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 0.8,
     },
   ]
